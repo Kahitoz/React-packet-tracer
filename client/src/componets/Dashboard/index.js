38ae790cf0485321040cf57d043926d32687f060
Kshitiz-Sinha <SINHA252001KSHITIZ@GMAIL.COM>
2023-04-13 23:51:24 +0530

    some changes

--- client/src/componets/Dashboard/index.js
+++ client/src/componets/Dashboard/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./Dashboard.css"
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
   const email = localStorage.getItem("userEmail"); // retrieve email from localStorage
   const name = email ? `Welcome  ${email}` : "Welcome Back";
 
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("userEmail"); // remove email from localStorage
+    navigate("/login"); // navigate to login page
+  };
+
   return (
     <div>
-      <div>
+      <div className="box">
         <p className="Text">{name}</p>
+        
       </div>
+      <button onClick={handleLogout} className="button">Logout</button>
     </div>
   );
 }
